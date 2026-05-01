79e836ecfa57dda54b87d70bdf4af8ee45c82840
yasithwimukthi <wimukthiyasith@gmail.com>
2026-05-01 21:36:15 +0530

    Initial commit

---
+++ src/test/java/com/microservices/structuredlogsorderservice/StructuredLogsOrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.structuredlogsorderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructuredLogsOrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
