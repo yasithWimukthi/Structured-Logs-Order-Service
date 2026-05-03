932781165ca817e41c7df347a526e1734b0f5009
yasithwimukthi <wimukthiyasith@gmail.com>
2026-05-03 23:16:07 +0530

    create order controller

--- src/main/java/com/microservices/structuredlogsorderservice/controller/OrderController.java
+++ src/main/java/com/microservices/structuredlogsorderservice/controller/OrderController.java
@@ -1,5 +1,7 @@
 package com.microservices.structuredlogsorderservice.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,13 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/orders")
 public class OrderController {
 
+    private static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
     @GetMapping
     public String getOrders() {
+        log.info("Fetching all orders");
         return "Order Service Response";
     }
 
     @PostMapping
     public String createOrder() {
+        log.info("Creating new order");
         return "Order Created Successfully";
     }
 }
